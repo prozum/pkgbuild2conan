8a4bd7bd34da18796fcf02405574f792c3049e14
Niclas Moeslund Overby <noverby@prozum.dk>
2020-06-16 17:21:14 +0200

    Greatly simplify generated Conan recipes

--- pkgbuild2conan.sh
+++ pkgbuild2conan.sh
@@ -27,42 +27,37 @@ build=$(declare -f build | sed -e '1,2d;$ d;s/^[ \t]*//')
 case "$build" in
   *cmake*)
     build_system="cmake"
-    import_build_system="CMake, "
     ;;
   *configure*)
     build_system="autotools"
-    import_build_system="AutoToolsBuildEnvironment, "
     ;;
   *meson*)
     build_system="meson"
-    import_build_system="Meson, "
     ;;
   *setup.py*)
     build_system="python"
-    import_build_system=""
     ;;
   *)
     build_system="make"
-    import_build_system="AutoToolsBuildEnvironment, "
     ;;
 esac
 
-echo -e "import os"
-echo -e ""
-echo -e "from conans import ConanFile, ${import_build_system}tools"
+echo -e "from conans import *"
 echo -e ""
 echo -e "class ${pkgname_camel}Conan(ConanFile):"
 echo -e "    name = \"$pkgname\""
-echo -e "    version = tools.get_env(\"GIT_TAG\", \"$pkgver\")"
 echo -e "    description = \"$pkgdesc\""
 echo -e "    license = \"$license\""
-echo -e "    settings = \"os\", \"arch\", \"compiler\", \"build_type\""
-echo -e ""
-echo -e "    def build_requirements(self):"
-echo -e "        self.build_requires(\"generators/1.0.0@{}/stable\".format(self.user))"
+echo -en "    settings = {\"os\": [\"Linux\"]"
+if [ "${arch[0]}" != "any" ]; then
+  echo -en ", \"arch\": [\"x86_64\", \"armv8\"]"
+fi
+echo -e "}"
+echo -e "    build_requires = ("
+echo -e "        \"generators/1.0.0\","
 case "$build_system" in
   autotools|make)
-    echo -e "        self.build_requires(\"autotools/1.0.0@{}/stable\".format(self.user))"
+    echo -e "        \"autotools/1.0.0\","
     ;;
 esac
 for dep in "${makedepends[@]}"
@@ -72,13 +67,13 @@ do
     wget https://git.archlinux.org/svntogit/community.git/plain/trunk/PKGBUILD?h=packages/$dep -qO /tmp/$dep.PKGBUILD
   fi
   depver=$(grep pkgver= /tmp/$dep.PKGBUILD | cut -b 8-)
-	echo -e "        self.build_requires(\"$dep/[>=$depver]@{}/stable\".format(self.user))"
+	echo -e "        \"$dep/[>=$depver]\","
 done
-echo -e ""
+echo -e "    )"
 
 
 if [[ ${#depends[@]} -ne 0 ]]; then
-  echo -e "    def requirements(self):"
+  echo -e "    requires = ("
   for dep in "${depends[@]}"
   do
     wget https://git.archlinux.org/svntogit/packages.git/plain/trunk/PKGBUILD?h=packages/$dep -qO /tmp/$dep.PKGBUILD
@@ -86,21 +81,22 @@ if [[ ${#depends[@]} -ne 0 ]]; then
       wget https://git.archlinux.org/svntogit/community.git/plain/trunk/PKGBUILD?h=packages/$dep -qO /tmp/$dep.PKGBUILD
     fi
     depver=$(grep pkgver= /tmp/$dep.PKGBUILD | cut -b 8-)
-  	echo -e "        self.requires(\"$dep/[>=$depver]@{}/stable\".format(self.user))"
+  	echo -e "        \"$dep/[>=$depver]\","
   done
+  echo -e "    )"
   echo -e ""
 fi
 
-download=$(echo "${source[0]}" | sed "s/$pkgver/{0}/g")
+download=$(echo "${source[0]}" | sed "s/$pkgver/{self.version}/g")
 echo -e "    def source(self):"
-echo -e "        tools.get(\"$download\".format(self.version))"
+echo -e "        tools.get(f\"$download\")"
 echo -e ""
 
 echo -e "    def build(self):"
 case "$build_system" in
   cmake)
     echo -e "        cmake = CMake(self, generators=\"Ninja\")"
-    echo -e "        cmake.configure(source_folder=\"{}-{}\".format(self.name, self.version))"
+    echo -e "        cmake.configure(source_folder=f\"{self.name}-{self.version}\")"
     echo -e "        cmake.build()"
     echo -e "        cmake.install()"
     ;;
@@ -108,23 +104,22 @@ case "$build_system" in
     echo -e "        args = ["
     echo -e "            \"--disable-static\","
     echo -e "        ]"
-    echo -e "        with tools.chdir(\"{}-{}\".format(self.name, self.version)):"
-    echo -e "            autotools = AutoToolsBuildEnvironment(self)"
-    echo -e "            autotools.configure(args=args)"
-    echo -e "            autotools.make()"
-    echo -e "            autotools.install()"
+    echo -e "        autotools = AutoToolsBuildEnvironment(self)"
+    echo -e "        autotools.configure(args=args, configure_dir=f\"{self.name}-{self.version}\")"
+    echo -e "        autotools.make()"
+    echo -e "        autotools.install()"
     ;;
   meson)
     echo -e "        meson = Meson(self)"
-    echo -e "        meson.configure(source_folder=\"{}-{}\".format(self.name, self.version))"
+    echo -e "        meson.configure(source_folder=f\"{self.name}-{self.version}\")"
     echo -e "        meson.install()"
     ;;
   python)
-    echo -e "        with tools.chdir(\"{}-{}\".format(self.name, self.version)):"
-    echo -e "            self.run('python setup.py install --optimize=1 --prefix= --root=\"{}\"'.format(self.package_folder))"
+    echo -e "        with tools.chdir(f\"{self.name}-{self.version}\"):"
+    echo -e "            self.run(f'python setup.py install --optimize=1 --prefix= --root=\"{self.package_folder}\"')"
     ;;
   make)
-    echo -e "        with tools.chdir(\"{}-{}\".format(self.name, self.version)):"
+    echo -e "        with tools.chdir(f\"{self.name}-{self.version}\"):"
     echo -e "            autotools = AutoToolsBuildEnvironment(self)"
     echo -e "            autotools.make()"
     echo -e "            autotools.install()"
