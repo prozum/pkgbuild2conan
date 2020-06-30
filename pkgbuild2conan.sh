b593f5adb382986fd251f314dbc587ff344d5065
Niclas Overby <noverby@prozum.dk>
2020-06-30 13:27:34 +0200

    Add support for requirements and npm

--- pkgbuild2conan.sh
+++ pkgbuild2conan.sh
@@ -51,15 +51,28 @@ case "$build" in
   *setup.py*)
     build_system="python"
     ;;
+  *npm*)
+    build_system="npm"
+    ;;
   *)
     build_system="make"
     ;;
 esac
 
+# Handle npm packages without build function
+package=$(declare -f package | sed -e '1,2d;$ d;s/^[ \t]*//')
+case "$package" in
+  *npm*)
+    if [ "$build_system" == "make" ]; then
+      build_system="npm"
+    fi
+    ;;
+esac
+
+# Output Conan recipe
 echo -e "from conans import *"
 echo -e ""
 echo -e "class ${pkgname_camel}Conan(ConanFile):"
-echo -e "    name = \"$pkgname\""
 echo -e "    description = \"$pkgdesc\""
 echo -e "    license = \"$license\""
 echo -en "    settings = {\"os\": [\"Linux\"]"
@@ -67,43 +80,52 @@ if [ "${arch[0]}" != "any" ]; then
   echo -en ", \"arch\": [\"x86_64\", \"armv8\"]"
 fi
 echo -e "}"
-echo -e "    build_requires = ("
-echo -e "        \"generators/1.0.0\","
-case "$build_system" in
-  autotools|make)
-    echo -e "        \"autotools/1.0.0\","
-    ;;
-esac
-for dep in "${makedepends[@]}"
-do
-  wget https://git.archlinux.org/svntogit/packages.git/plain/trunk/PKGBUILD?h=packages/$dep -qO /tmp/$dep.PKGBUILD
-  if [[ $? -ne 0 ]]; then
-    wget https://git.archlinux.org/svntogit/community.git/plain/trunk/PKGBUILD?h=packages/$dep -qO /tmp/$dep.PKGBUILD
-  fi
-  depver=$(grep pkgver= /tmp/$dep.PKGBUILD | cut -b 8-)
-	echo -e "        \"$dep/[>=$depver]\","
-done
-echo -e "    )"
-
 
 if [[ ${#depends[@]} -ne 0 ]]; then
-  echo -e "    requires = ("
-  for dep in "${depends[@]}"
+  echo -e "    build_requires = ("
+  case "$build_system" in
+    autotools|make)
+      echo -e "        \"autotools/[^1.0.0]\","
+      ;;
+  esac
+
+  for dep in "${makedepends[@]}"
   do
     wget https://git.archlinux.org/svntogit/packages.git/plain/trunk/PKGBUILD?h=packages/$dep -qO /tmp/$dep.PKGBUILD
     if [[ $? -ne 0 ]]; then
       wget https://git.archlinux.org/svntogit/community.git/plain/trunk/PKGBUILD?h=packages/$dep -qO /tmp/$dep.PKGBUILD
     fi
     depver=$(grep pkgver= /tmp/$dep.PKGBUILD | cut -b 8-)
-  	echo -e "        \"$dep/[>=$depver]\","
+  	echo -e "        \"$dep/[^$depver]\","
   done
   echo -e "    )"
 fi
 
+echo -e "    requires = ("
+echo -e "        \"generators/[^1.0.0]\","
+for dep in "${depends[@]}"
+do
+  wget https://git.archlinux.org/svntogit/packages.git/plain/trunk/PKGBUILD?h=packages/$dep -qO /tmp/$dep.PKGBUILD
+  if [[ $? -ne 0 ]]; then
+    wget https://git.archlinux.org/svntogit/community.git/plain/trunk/PKGBUILD?h=packages/$dep -qO /tmp/$dep.PKGBUILD
+  fi
+  depver=$(grep pkgver= /tmp/$dep.PKGBUILD | cut -b 8-)
+	echo -e "        \"$dep/[^$depver]\","
+done
+echo -e "    )"
+
 download=$(echo "${source[0]}" | sed "s/$pkgver/{self.version}/g")
 echo -e ""
 echo -e "    def source(self):"
-echo -e "        tools.get(f\"$download\")"
+
+case "$build_system" in
+  npm)
+    echo -e "        tools.download(f\"$download\", f\"{self.name}-{self.version}.tgz\")"
+    ;;
+  *)
+    echo -e "        tools.get(f\"$download\")"
+    ;;
+esac
 
 echo -e ""
 echo -e "    def build(self):"
@@ -131,6 +153,9 @@ case "$build_system" in
   python)
     echo -e "        with tools.chdir(f\"{self.name}-{self.version}\"):"
     echo -e "            self.run(f'python setup.py install --optimize=1 --prefix= --root=\"{self.package_folder}\"')"
+    ;;
+  npm)
+    echo -e "        self.run(f'npm install -g --user root --prefix \"{self.package_folder}\" \"{self.name}-{self.version}.tgz\"')"
     ;;
   make)
     echo -e "        with tools.chdir(f\"{self.name}-{self.version}\"):"
