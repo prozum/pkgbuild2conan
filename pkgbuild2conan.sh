74f1c1da4122838c33e46d03d1ae0a2331094de9
Niclas Overby <noverby@prozum.dk>
2020-07-03 15:39:28 +0200

    Add alpine support

--- pkgbuild2conan.sh
+++ pkgbuild2conan.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# Use alpine packages
+if [ "$1" == "-a" ]; then
+    alpine=1
+    shift
+fi
+
 # Print config.yml argument
 if [ "$1" == "-c" ]; then
     print_config=1
@@ -8,21 +14,30 @@ fi
 
 if [[ -z "$1" ]] ; then
     echo "No argument supplied!"
-    echo "Usage: pkgbuild2conan [-c] PKGBUILD-NAME"
+    echo "Usage: pkgbuild2conan [-c] PKG-NAME"
     exit 1
 fi
 
+# Handle + in package name
 pkg="${1//+/%2B}"
 
-# Load PKGBUILD
-wget https://git.archlinux.org/svntogit/packages.git/plain/trunk/PKGBUILD?h=packages/$pkg -qO /tmp/$pkg.PKGBUILD
-if [[ $? -ne 0 ]]; then
-  wget https://git.archlinux.org/svntogit/community.git/plain/trunk/PKGBUILD?h=packages/$pkg -qO /tmp/$pkg.PKGBUILD
+# Load package
+if [ -n "$alpine" ]; then
+  wget https://git.alpinelinux.org/aports/plain/main/$pkg/APKBUILD -qO /tmp/$pkg.PKG
+  if [[ $? -ne 0 ]]; then
+    wget https://git.alpinelinux.org/aports/plain/community/$pkg/APKBUILD -qO /tmp/$pkg.PKG
+  fi
+  source /tmp/$pkg.PKG
+else
+  wget https://git.archlinux.org/svntogit/packages.git/plain/trunk/PKGBUILD?h=packages/$pkg -qO /tmp/$pkg.PKG
+  if [[ $? -ne 0 ]]; then
+    wget https://git.archlinux.org/svntogit/community.git/plain/trunk/PKGBUILD?h=packages/$pkg -qO /tmp/$pkg.PKG
+  fi
+  if [[ $? -ne 0 ]]; then
+    wget https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=$pkg -qO /tmp/$pkg.PKG
+  fi
+  source /tmp/$pkg.PKG
 fi
-if [[ $? -ne 0 ]]; then
-  wget https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=$pkg -qO /tmp/$pkg.PKGBUILD
-fi
-source /tmp/$pkg.PKGBUILD
 
 # Print config.yml
 if [[ $print_config -eq 1 ]]; then
@@ -93,29 +108,60 @@ if [[ ${#depends[@]} -ne 0 ]]; then
       ;;
   esac
 
-  for dep in "${makedepends[@]}"
-  do
-    wget https://git.archlinux.org/svntogit/packages.git/plain/trunk/PKGBUILD?h=packages/$dep -qO /tmp/$dep.PKGBUILD
-    if [[ $? -ne 0 ]]; then
-      wget https://git.archlinux.org/svntogit/community.git/plain/trunk/PKGBUILD?h=packages/$dep -qO /tmp/$dep.PKGBUILD
-    fi
-    depver=$(grep pkgver= /tmp/$dep.PKGBUILD | cut -b 8-)
-  	echo -e "        \"$dep/[^$depver]\","
-  done
+  if [ -n "$alpine" ]; then
+    for dep in $makedepends
+    do
+      # Don't use dev packages
+      dep=${dep/%-dev/}
+
+      wget https://git.alpinelinux.org/aports/plain/main/$dep/APKBUILD -qO /tmp/$dep.PKG
+      if [[ $? -ne 0 ]]; then
+        wget https://git.alpinelinux.org/aports/plain/community/$dep/APKBUILD -qO /tmp/$dep.PKG
+      fi
+      depver=$(grep pkgver= /tmp/$dep.PKG | cut -b 8-)
+    	echo -e "        \"$dep/[^$depver]\","
+    done
+  else
+    for dep in "${makedepends[@]}"
+    do
+      wget https://git.archlinux.org/svntogit/packages.git/plain/trunk/PKGBUILD?h=packages/$dep -qO /tmp/$dep.PKG
+      if [[ $? -ne 0 ]]; then
+        wget https://git.archlinux.org/svntogit/community.git/plain/trunk/PKGBUILD?h=packages/$dep -qO /tmp/$dep.PKG
+      fi
+      depver=$(grep pkgver= /tmp/$dep.PKG | cut -b 8-)
+    	echo -e "        \"$dep/[^$depver]\","
+    done
+  fi
   echo -e "    )"
 fi
 
 echo -e "    requires = ("
 echo -e "        \"generators/[^1.0.0]\","
-for dep in "${depends[@]}"
-do
-  wget https://git.archlinux.org/svntogit/packages.git/plain/trunk/PKGBUILD?h=packages/$dep -qO /tmp/$dep.PKGBUILD
-  if [[ $? -ne 0 ]]; then
-    wget https://git.archlinux.org/svntogit/community.git/plain/trunk/PKGBUILD?h=packages/$dep -qO /tmp/$dep.PKGBUILD
-  fi
-  depver=$(grep pkgver= /tmp/$dep.PKGBUILD | cut -b 8-)
-	echo -e "        \"$dep/[^$depver]\","
-done
+
+if [ -n "$alpine" ]; then
+  for dep in $depends
+  do
+    # Don't use dev packages
+    dep=${dep/%-dev/}
+
+    wget https://git.alpinelinux.org/aports/plain/main/$dep/APKBUILD -qO /tmp/$dep.PKG
+    if [[ $? -ne 0 ]]; then
+      wget https://git.alpinelinux.org/aports/plain/community/$dep/APKBUILD -qO /tmp/$dep.PKG
+    fi
+    depver=$(grep pkgver= /tmp/$dep.PKG | cut -b 8-)
+  	echo -e "        \"$dep/[^$depver]\","
+  done
+else
+  for dep in "${depends[@]}"
+  do
+    wget https://git.archlinux.org/svntogit/packages.git/plain/trunk/PKGBUILD?h=packages/$dep -qO /tmp/$dep.PKG
+    if [[ $? -ne 0 ]]; then
+      wget https://git.archlinux.org/svntogit/community.git/plain/trunk/PKGBUILD?h=packages/$dep -qO /tmp/$dep.PKG
+    fi
+    depver=$(grep pkgver= /tmp/$dep.PKG | cut -b 8-)
+  	echo -e "        \"$dep/[^$depver]\","
+  done
+fi
 echo -e "    )"
 
 download=$(echo "${source[0]}" | sed "s/$pkgver/{self.version}/g")
